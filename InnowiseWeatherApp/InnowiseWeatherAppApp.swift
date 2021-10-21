2c6d4b29530e59abbf9be4e7432de09f680ce4f5
Matthew <83066475+MatFatCat@users.noreply.github.com>
2021-10-21 21:01:32 +0300

    Add a project

---
+++ InnowiseWeatherApp/InnowiseWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnowiseWeatherAppApp.swift
+//  InnowiseWeatherApp
+//
+//  Created by Matthew Popov on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct InnowiseWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
