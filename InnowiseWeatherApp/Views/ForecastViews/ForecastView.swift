112cd3cae9fd0237c9c48457825208eccf3328d1
Matthew <83066475+MatFatCat@users.noreply.github.com>
2021-10-27 21:38:57 +0300

    add loading view

--- InnowiseWeatherApp/Views/ForecastViews/ForecastView.swift
+++ InnowiseWeatherApp/Views/ForecastViews/ForecastView.swift
@@ -13,17 +13,20 @@ struct ForecastView: View {
     @EnvironmentObject var locationViewModel: LocationViewModel
     @State var forecast: Forecast = Forecast()
     @State var networkManager: NetworkManager = NetworkManager(lat: 0.0, lon: 0.0)
+    @State var isLoading = true
     
     var coordinate: CLLocationCoordinate2D? {
         return locationViewModel.lastSeenLocation?.coordinate
     }
 
     var body: some View {
-        VStack{
-            GeometryReader { geometry in
-                if self.forecast.list != nil {
-                    List(0..<self.forecast.list!.count) { i in
-                        ForecastRawView(temp: self.forecast.list![i].main!.temp!, iconPath: self.forecast.list![i].weather![0].icon!, description: self.forecast.list![i].weather![0].description!, geometry: geometry, txtDate: self.forecast.list![i].dt_txt!)
+        LoadingView(isShowing: self.$isLoading) {
+            VStack{
+                GeometryReader { geometry in
+                    if self.forecast.list != nil {
+                        List(0..<self.forecast.list!.count) { i in
+                            ForecastRawView(temp: self.forecast.list![i].main!.temp!, iconPath: self.forecast.list![i].weather![0].icon!, description: self.forecast.list![i].weather![0].description!, geometry: geometry, txtDate: self.forecast.list![i].dt_txt!)
+                        }
                     }
                 }
             }
@@ -34,6 +37,7 @@ struct ForecastView: View {
             
             self.networkManager.getData { (dataFromAPI) in
                 self.forecast = dataFromAPI
+                self.isLoading = false
             }
         }
     }
